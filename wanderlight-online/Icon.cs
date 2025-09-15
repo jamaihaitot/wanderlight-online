783d58f356ef84820be92853f18c00d195e09c93
Josef Jamai-Haitot <jamaihaitot.j@gmail.com>
2025-09-15 23:21:49 +0200

    Fix analyzers: add namespace and XML docs to Icon; update dotnet: format task to target solution

--- wanderlight-online/Icon.cs
+++ wanderlight-online/Icon.cs
@@ -1,15 +1,29 @@
-using Godot;
-using System;
+// <copyright file="Icon.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
-public partial class Icon : Sprite2D
+namespace WanderlightOnline
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    using Godot;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    /// <summary>
+    /// Basic Sprite2D node script used by the project template.
+    /// </summary>
+    public partial class Icon : Sprite2D
+    {
+        /// <summary>
+        /// Called when the node enters the scene tree for the first time.
+        /// </summary>
+        public override void _Ready()
+        {
+        }
+
+        /// <summary>
+        /// Called every frame with the elapsed time since the previous frame.
+        /// </summary>
+        /// <param name="delta">Elapsed time since the previous frame, in seconds.</param>
+        public override void _Process(double delta)
+        {
+        }
+    }
 }
